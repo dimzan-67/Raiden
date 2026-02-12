cfdb41435296935f03db16c79498a004bb05f6eb
DimZan <stobamnotes@icloud.com>
2026-02-12 18:07:22 +0200

    Update raiden-loader.lua

--- raiden-loader.lua
+++ raiden-loader.lua
@@ -16,10 +16,10 @@ if gameName:find("rivals") then
     local executor = (identifyexecutor and identifyexecutor()) or "Unknown"
     local executor_lower = executor:lower()
     if executor_lower:find("xeno") or executor_lower:find("jjsploit") or executor_lower:find("drift") or executor_lower:find("solara") then
-        loadstring(game:HttpGet("https://smile-hub.vercel.app/rivals-external.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dimzan-67/Raiden/refs/heads/main/raiden-rivals-external.lua"))()
     else
-        loadstring(game:HttpGet("https://smile-hub.vercel.app/rivals.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/dimzan-67/Raiden/refs/heads/main/raiden-rivals.lua"))()
     end
 elseif gameName:find("escape tsunami for brainrots") then
-    loadstring(game:HttpGet("https://smile-hub.vercel.app/etfb.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dimzan-67/Raiden/refs/heads/main/raiden-etfb.lua"))()
 end

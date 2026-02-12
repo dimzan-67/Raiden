6048f9e2f0835e4a3897ef942a5fcc5dbfaf92e9
DimZan <stobamnotes@icloud.com>
2026-02-12 18:06:30 +0200

    Create raiden-loader.lua

---
+++ raiden-loader.lua
@@ -0,0 +1,25 @@
+--[[
+  _____       _     _            
+ |  __ \     (_)   | |           
+ | |__) |__ _ _  __| | ___ _ __  
+ |  _  // _` | |/ _` |/ _ \ '_ \ 
+ | | \ \ (_| | | (_| |  __/ | | |
+ |_|  \_\__,_|_|\__,_|\___|_| |_|
+
+ ]]
+
+local MarketplaceService = game:GetService("MarketplaceService")
+local gameInfo = MarketplaceService:GetProductInfo(game.PlaceId)
+local gameName = gameInfo.Name:lower()
+
+if gameName:find("rivals") then
+    local executor = (identifyexecutor and identifyexecutor()) or "Unknown"
+    local executor_lower = executor:lower()
+    if executor_lower:find("xeno") or executor_lower:find("jjsploit") or executor_lower:find("drift") or executor_lower:find("solara") then
+        loadstring(game:HttpGet("https://smile-hub.vercel.app/rivals-external.lua"))()
+    else
+        loadstring(game:HttpGet("https://smile-hub.vercel.app/rivals.lua"))()
+    end
+elseif gameName:find("escape tsunami for brainrots") then
+    loadstring(game:HttpGet("https://smile-hub.vercel.app/etfb.lua"))()
+end
